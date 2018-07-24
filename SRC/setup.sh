1bfea671cf021640850af1eedfd4cec0ccbca722
Marcelo Bianchi <m.bianchi@iag.usp.br>
2018-07-24 18:13:29 -0300

    patch for gcc 6

--- SRC/setup.sh
+++ SRC/setup.sh
@@ -5,8 +5,11 @@ echo "*** Getting codes ***"
 wget -q -O - 'http://www.lorraine-lisiecki.com/match-2.3.1.tgz' | tar xvzf - | awk '{print " ",$0}'
 (
 	cd Match-2.3.1/ || exit
-	echo "*** Patching ***"
-	cat ../patch.gcc-4.9 | patch -p1 | awk '{print " ",$0}'
+	echo "*** Patching (using patch for gcc version 6.3, change to 4.9 if you have issues) ***"
+
+	## cat ../patch.gcc-4.9 | patch -p1 | awk '{print " ",$0}'
+	cat ../patch.gcc-6.3 | patch -p1 | awk '{print " ",$0}'
+	
 	cd Match/ || exit
 	echo "*** Compiling ***"
 	make clean | awk '{print " ",$0}'
